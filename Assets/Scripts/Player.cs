d1c88a18b1884562a6b3b8453a4163515b8d1c23
李一帆 <liyifan@liyifandeMacBook-Pro.local>
2020-03-11 03:44:31 +0800

    Basic setup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Players : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float playerSpeed = 10f;
+    [SerializeField] float jumpPower = 10f;
+
+    float m_XMove;
+    bool m_isFacingRight = true;
+    Rigidbody2D m_rigidbody2D;
+
     void Start()
     {
-        
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Control
+        m_XMove = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        // Animation
+
+        // Direction
+        if (m_XMove < 0f && m_isFacingRight == true)
+        {
+            FlipPlayer();
+        }
+        else if (m_XMove > 0f && m_isFacingRight == false)
+        {
+            FlipPlayer();
+        }
+
+        // Physics
+        var currentVelocity = m_rigidbody2D.velocity;
+        m_rigidbody2D.velocity = new Vector2(m_XMove * playerSpeed * Time.deltaTime, currentVelocity.y);
+        //transform.Translate(Vector3.right * m_XMove * playerSpeed * Time.deltaTime);
+
+    }
+
+    private void Jump()
+    {
+        m_rigidbody2D.AddForce(Vector2.up * jumpPower);
+    }
+
+    private void FlipPlayer()
+    {
+        m_isFacingRight = !m_isFacingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 }
